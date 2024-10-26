5240ed932f58e0ce3bcc6ffd9362243764767e89
Nastasya <Anyrise@mail.ru>
2024-10-26 10:32:36 +0300

    Moved the method "readFromFile" to the FileUtils class

--- sworld/src/Main.java
+++ sworld/src/Main.java
@@ -32,7 +32,7 @@ public class Main {
             } else if (userInput < 0) {
                 System.out.println("Wrong option. Try again");
             } else if (userInput == 2) {
-                readFromFile();
+                FileUtils.readFromFile();
             } else if (userInput == 1) {
                 writeToFile();
             }
@@ -83,32 +83,6 @@ public class Main {
 
         } catch (IOException e) {
             System.out.println("Sorry, cannot write to file. Write something else");
-
-        }
-    }
-
-    public static void readFromFile() {
-        try {
-            File userfile = new File("C:\\Users\\AnastasiaBabanskay\\ufile.txt");
-            Scanner filescanner = new Scanner(userfile);
-            ArrayList<String> dataFromFile = new ArrayList<>();
-
-
-            while (filescanner.hasNextLine()) {
-                dataFromFile.add(filescanner.nextLine());
-            }
-
-            String myShirt = String.format("\nShirt color: %s \nShirt size: %s \nShirt material: %s" +
-                            "\nButton size: %s \nButton color: %s\nButton material: %s \nSleeves have buttons: %s",
-                    dataFromFile.get(0), dataFromFile.get(1), dataFromFile.get(2), dataFromFile.get(3), dataFromFile.get(4),
-                    dataFromFile.get(5), dataFromFile.get(6));
-
-            System.out.println("My shirt" + myShirt);
-
-            System.out.println("=============================");
-
-        } catch (FileNotFoundException e) {
-            System.out.println("Sorry, file doesn't exist. Create a file first");
 
         }
 
